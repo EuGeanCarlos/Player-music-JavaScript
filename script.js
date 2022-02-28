2d0ec2d1b68839a87f5cda05d1bbca397fe1305c
Gean Carlos <88750180+EuGeanCarlos@users.noreply.github.com>
2022-02-28 15:57:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let musica = document.querySelector('audio');
-
+// EVENTOS
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
+musica.addEventListener('timeupdate', atualizarBarra);
+
+// FUNÇÕES 
 function tocarMusica(){
     musica.play();
     document.querySelector('.botao-pause').style.display ='block';
@@ -12,4 +15,10 @@ function pausarMusica(){
     musica.pause();
     document.querySelector('.botao-pause').style.display ='none';
     document.querySelector('.botao-play').style.display ='block';
+}
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
