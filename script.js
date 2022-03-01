b4a709df6618d3023c1a0bf3e17fa54cbcb82859
Gean Carlos <88750180+EuGeanCarlos@users.noreply.github.com>
2022-03-01 16:42:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,79 @@
-let musica = [
+let musicas = [
     {titulo:'Born a Rockstar - NEFFEX', artista:'Gean Carlos', src:'music/Born a Rockstar - NEFFEX.mp3', img:'img/rock.jpg'},
     {titulo:'Smokey s Lounge - TrackTribe', artista:'Arnold Cruz', src:'music/Smokeys Lounge - TrackTribe.mp3', img:'img/jazz.jpg'},
     {titulo:'Like That - Anno Domini Beats', artista:'Lil Zyvk', src:'music/Like That - Anno Domini Beats.mp3', img:'img/trap.jpg'}
 
 ];
 
-
 let musica = document.querySelector('audio');
 let indexMusica = 0;
 
-// EVENTOS
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+// Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
-let duracaoMusica = document.querySelector('.fim');
-
-let imagem = document.querySelector('img');
-
-let nomeMusica = document.querySelector('.decricao h2');
-
-let nomeArtista = document.querySelector('.descricao i'); 
-
-
-duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
 musica.addEventListener('timeupdate', atualizarBarra);
 
 document.querySelector('.anterior').addEventListener('click', () => {
     indexMusica--;
-    redenrizarMusica();
-
-});
-document.querySelector('.proximo').addEventListener('click', () => {
-    indexMusica++;    
-    redenrizarMusica();
-
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
 });
 
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
 
-// FUNÇÕES 
-
-function redenrizarMusica(index){
-    musica.setAttribute('src',musica[index].src );
+// Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
     musica.addEventListener('loadeddata', () => {
-        nomeMusica.textContent = musica[index].titulo;
-        nomeArtista.textContent = musica[index].artista;
-        imagem.src = musica[index].img;
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
         duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
     });
 }
 
 function tocarMusica(){
     musica.play();
-    document.querySelector('.botao-pause').style.display ='block';
-    document.querySelector('.botao-play').style.display ='none';
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
 }
+
 function pausarMusica(){
     musica.pause();
-    document.querySelector('.botao-pause').style.display ='none';
-    document.querySelector('.botao-play').style.display ='block';
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
 }
+
 function atualizarBarra(){
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
     let tempoDecorrido = document.querySelector('.inicio');
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
+
 function segundosParaMinutos(segundos){
     let campoMinutos = Math.floor(segundos / 60);
     let campoSegundos = segundos % 60;
     if (campoSegundos < 10){
         campoSegundos = '0' + campoSegundos;
     }
+
     return campoMinutos+':'+campoSegundos;
 }
-
